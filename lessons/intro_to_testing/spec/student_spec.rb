f8dca01b9f618c99c729791f9c3cb9db3432cd64
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-08-20 13:21:57 -0700

    Testing Run1

--- lessons/intro_to_testing/spec/student_spec.rb
+++ lessons/intro_to_testing/spec/student_spec.rb
@@ -1,13 +1,33 @@
 # Add the 2 requires you will need here
-
+require './lib/student'
 
 describe Student do
-  describe '#initialize' do
+  describe '#initialize' do #the name of the method that we are testing
     it 'is an instance of student' do
       # write the code to initialize a new student object
+      student = Student.new("Jonathan")
+    
+      expect(student.name) to be eq("Jonathan")
     end
     # test it has a name
+    it 'is given a name' do 
+      student = Student.new("Jonathan")
+      
+      expect(student.name) .to eq("Jonathan")
+    end  
     # test it has cookies
+    it 'has an empty array of cookies' do
+      student = Student.new("Jonathan")
+
+      expect(student.cookies) .to eq([])
+    end
     # test it can add cookies
+    describe 'Add cookie' do
+      it "adds a cookie to our @cookies list" do
+        student = Student.new("Jonathan")
+        student.add_cookie("Chocolate Chip")
+        student.add_cookie("Peanut Butter")
+      expect(student.cookies) .to eq(["Chocolate Chip", "Peanut Butter"])
+      end 
   end
 end
